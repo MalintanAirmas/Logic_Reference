6bbfd2d85b599146b13a34622ec0c796131b28e0
Hernawan Putra Malintan <hernawan@airmasgroup.co.id>
2021-03-31 17:01:56 +0700

    logic 2

---
+++ logic2.js
@@ -0,0 +1,6 @@
+var limit = "Halo nama saya hernawan, Halo nama saya hernawan,Halo nama saya hernawan, Halo nama saya hernawan,hi";
+var init ="a";
+
+for (let index = init.length; index <= limit.length; index++) {
+      console.log (index);
+  }
